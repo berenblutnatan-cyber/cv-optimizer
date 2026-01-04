664b7b945e3069171cc66a6ee53b8e50cf67cfd8
Your Name <berenblutnatan@gmail.com>
2026-01-04 20:26:17 +0200

    Add global CV optimized counter

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import { HomeClient } from "@/components/HomeClient";
 import { getCvOptimizedCount } from "@/lib/kv";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default async function HomePage() {
   const count = await getCvOptimizedCount();
   return <HomeClient initialCount={count} />;
